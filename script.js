dc5c157a968727d8af794cf507dc4fcca41561d5
Josué Carvajal <josue.crs97@gmail.com>
2020-07-31 15:58:30 -0600

    August update

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function getDataByProvince(myData, province_id, resultDivName) {
 function renderData(element, resultDivName) {
     //clear both screens to avoid issues with the two results div
     clearScreen("results")
-    clearScreen("results2")
+        //clearScreen("results2")
         //title
     createTitleElement(element.provincia, resultDivName)
         //list container
@@ -80,138 +80,8 @@ function clearScreen(resultDivName) {
         resultsNode.firstChild.remove();
     }
 }
-//this DATA will be updated every day. 
+//this DATA will be updated every month. 
 DATA = [{
-        "id": 1,
-        "provincia": "San José",
-        "alerta": {
-            "naranja": [
-                { "canton": "San José", "gam": true },
-                { "canton": "Escazú", "gam": true },
-                { "canton": "Aserrí", "gam": true },
-                { "canton": "Mora", "gam": true },
-                { "canton": "Goicoechea", "gam": true },
-                { "canton": "Santa Ana", "gam": true },
-                { "canton": "Alajuelita", "gam": true },
-                { "canton": "Vásquez de Coronado", "gam": true },
-                { "canton": "Tibás", "gam": true },
-                { "canton": "Moravia", "gam": true },
-                { "canton": "Montes de Oca", "gam": true },
-                { "canton": "Curridabat", "gam": true },
-                { "canton": "Desamparados", "gam": true },
-                { "canton": "Puriscal", "gam": false }
-            ]
-        },
-        "actualizado": "10/07/2020"
-    },
-    {
-        "id": 2,
-        "provincia": "Alajuela",
-        "alerta": {
-            "naranja": [
-                { "canton": "Alajuela", "gam": true },
-                { "canton": "Poás", "gam": true },
-                { "canton": "Atenas", "gam": true },
-                { "canton": "Grecia", "gam": true },
-                { "canton": "Sarchí", "gam": true },
-                { "canton": "Palmares", "gam": true },
-                { "canton": "Naranjo", "gam": true },
-                { "canton": "San Ramón", "gam": true },
-                { "canton": "Zarcero", "gam": true },
-                { "canton": "Upala", "gam": false },
-                { "canton": "Guatuso", "gam": false },
-                { "canton": "Los Chiles", "gam": false },
-                { "canton": "San Carlos (Tres Esquinas)", "gam": false },
-                { "canton": "San Carlos (Los Ángeles)", "gam": false },
-                { "canton": "San Carlos (Sonafluca)", "gam": false },
-                { "canton": "San Carlos (La Perla)", "gam": false },
-                { "canton": "San Carlos (San Isidro)", "gam": false },
-                { "canton": "San Carlos (El Tanque)", "gam": false },
-                { "canton": "San Carlos (San Jorge)", "gam": false },
-                { "canton": "San Carlos (Santa Cecilia)", "gam": false },
-                { "canton": "San Carlos (La Vega)", "gam": false },
-                { "canton": "San Carlos (Bonanza)", "gam": false }
-            ]
-        },
-        "actualizado": "10/07/2020"
-    },
-    {
-        "id": 3,
-        "provincia": "Heredia",
-        "alerta": {
-            "naranja": [
-                { "canton": "Heredia", "gam": true },
-                { "canton": "Barva", "gam": true },
-                { "canton": "Santo Domingo", "gam": true },
-                { "canton": "Santa Bárbara", "gam": true },
-                { "canton": "San Rafael", "gam": true },
-                { "canton": "San Isidro", "gam": true },
-                { "canton": "Belén", "gam": true },
-                { "canton": "Flores", "gam": true },
-                { "canton": "San Pablo", "gam": true }
-            ]
-        },
-        "actualizado": "10/07/2020"
-    },
-    {
-        "id": 4,
-        "provincia": "Cartago",
-        "alerta": {
-            "naranja": [
-                { "canton": "Cartago", "gam": true },
-                { "canton": "Paraíso", "gam": true },
-                { "canton": "La Unión", "gam": true },
-                { "canton": "Alvarado", "gam": true },
-                { "canton": "Oreamuno ", "gam": true },
-                { "canton": "El Guarco", "gam": true }
-            ]
-        },
-        "actualizado": "10/07/2020"
-    },
-    {
-        "id": 5,
-        "provincia": "Guanacaste",
-        "alerta": {
-            "naranja": [
-                { "canton": "Bagaces", "gam": false },
-                { "canton": "Liberia", "gam": false },
-                { "canton": "Carrillo", "gam": false },
-                { "canton": "Cañas", "gam": false }
-            ]
-        },
-        "actualizado": "10/07/2020"
-    },
-    {
-        "id": 6,
-        "provincia": "Puntarenas",
-        "alerta": {
-            "naranja": [
-                { "canton": "Golfito", "gam": false },
-                { "canton": "Garabito", "gam": false },
-                { "canton": "Puntarenas (salvo los distritos de Cobano, Lepanto, Monteverde,  Isla Chira, Isla Caballo, Isla San Lucas, Isla del Coco e Isla Venado) y Corredores.", "gam": false }
-
-            ]
-        },
-        "actualizado": "10/07/2020"
-    },
-    {
-        "id": 7,
-        "provincia": "Limón",
-        "alerta": {
-            "naranja": [
-                { "canton": "Pococí", "gam": false },
-                { "canton": "Talamanca", "gam": false },
-                { "canton": "Guácimo", "gam": false }
-            ]
-        },
-        "actualizado": "10/07/2020"
-    }
-]
-
-
-
-//From July 20 to July 31
-DATA2 = [{
         "id": 1,
         "provincia": "San José",
         "alerta": {
@@ -230,8 +100,7 @@ DATA2 = [{
                 { "canton": "Montes de Oca", "gam": true },
                 { "canton": "Curridabat", "gam": true }
             ]
-        },
-        "actualizado": "17/07/2020"
+        }
     },
     {
         "id": 2,
@@ -240,28 +109,9 @@ DATA2 = [{
             "naranja": [
                 { "canton": "Alajuela", "gam": true },
                 { "canton": "Poás", "gam": true },
-                { "canton": "Atenas", "gam": true },
-                { "canton": "Grecia", "gam": true },
-                { "canton": "Sarchí", "gam": true },
-                { "canton": "Palmares", "gam": true },
-                { "canton": "Naranjo", "gam": true },
-                { "canton": "San Ramón", "gam": true },
-                { "canton": "Upala", "gam": false },
-                { "canton": "Guatuso", "gam": false },
-                { "canton": "Los Chiles", "gam": false },
-                { "canton": "San Carlos (Tres Esquinas)", "gam": false },
-                { "canton": "San Carlos (Los Ángeles)", "gam": false },
-                { "canton": "San Carlos (Sonafluca)", "gam": false },
-                { "canton": "San Carlos (La Perla)", "gam": false },
-                { "canton": "San Carlos (San Isidro)", "gam": false },
-                { "canton": "San Carlos (El Tanque)", "gam": false },
-                { "canton": "San Carlos (San Jorge)", "gam": false },
-                { "canton": "San Carlos (Santa Cecilia)", "gam": false },
-                { "canton": "San Carlos (La Vega)", "gam": false },
-                { "canton": "San Carlos (Bonanza)", "gam": false }
+                { "canton": "Naranjo", "gam": true }
             ]
-        },
-        "actualizado": "17/07/2020"
+        }
     },
     {
         "id": 3,
@@ -271,65 +121,51 @@ DATA2 = [{
                 { "canton": "Heredia", "gam": true },
                 { "canton": "Barva", "gam": true },
                 { "canton": "Santo Domingo", "gam": true },
-                { "canton": "Santa Bárbara", "gam": true },
                 { "canton": "San Rafael", "gam": true },
                 { "canton": "San Isidro", "gam": true },
-                { "canton": "Belén", "gam": true },
                 { "canton": "Flores", "gam": true },
                 { "canton": "San Pablo", "gam": true }
             ]
-        },
-        "actualizado": "17/07/2020"
+        }
     },
     {
         "id": 4,
         "provincia": "Cartago",
         "alerta": {
             "naranja": [
-                { "canton": "Cartago", "gam": true },
-                { "canton": "Paraíso", "gam": true },
-                { "canton": "La Unión", "gam": true },
-                { "canton": "Alvarado", "gam": true },
-                { "canton": "Oreamuno ", "gam": true },
-                { "canton": "El Guarco", "gam": true }
+                { "canton": "La Unión", "gam": true }
             ]
-        },
-        "actualizado": "17/07/2020"
+        }
     },
     {
         "id": 5,
         "provincia": "Guanacaste",
         "alerta": {
             "naranja": [
-                { "canton": "Liberia", "gam": false },
-                { "canton": "Nicoya", "gam": false },
-                { "canton": "Cañas", "gam": false }
+                { "canton": "No hay cantones en alerta naranja :)", "gam": false }
             ]
-        },
-        "actualizado": "17/07/2020"
+        }
     },
     {
         "id": 6,
         "provincia": "Puntarenas",
         "alerta": {
             "naranja": [
-                { "canton": "Golfito", "gam": false },
-                { "canton": "Garabito", "gam": false },
-                { "canton": "Puntarenas (salvo los distritos de Cóbano, Lepanto, Monteverde,  Isla Chira, Isla Caballo, Isla San Lucas, Isla del Coco e Isla Venado), el distrito de Pavón en el cantón de Golfito, y los distritos de Agua Buena y Sabalito del cantón de Coto Brus.", "gam": false }
+                { "canton": "Corredores", "gam": false },
+                { "canton": "Coto Brus (distritos Agua Buena y Sabalito)", "gam": false },
+                { "canton": "Golfito (distrito Pavón)", "gam": false },
+                { "canton": "Puntarenas (distritos de Barranca y Chacarita)", "gam": false }
 
             ]
-        },
-        "actualizado": "17/07/2020"
+        }
     },
     {
         "id": 7,
         "provincia": "Limón",
         "alerta": {
             "naranja": [
-                { "canton": "Pococí", "gam": false },
-                { "canton": "Guácimo", "gam": false }
+                { "canton": "No hay cantones en alerta naranja :)", "gam": false }
             ]
-        },
-        "actualizado": "17/07/2020"
+        }
     }
 ]
